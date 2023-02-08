24526d8332efe95ef141da4e3358993c461bb6ba
Diogo Tomas <79857589+DiogoTomasM@users.noreply.github.com>
2023-02-08 18:46:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,4 @@ manda pro listador.py'''
 galera = []
 galera = [*csv.DictReader(open('nomes.csv', encoding='utf-8'))]
 cod = listador.licodificador(galera)
-cod = [7007028954, 7007124194, 7006833854, 6993480540, 7008135316, 0, 0, 7007028954, 43]
 cod = listador.avaliar(cod,galera)
