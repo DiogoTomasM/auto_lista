8dd6f296014d0ab118e0c2b59e9151dea2c1e044
Diogo Tomas <79857589+DiogoTomasM@users.noreply.github.com>
2023-02-08 16:03:49 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-id = "117667552"
-url = f"https://api.opendota.com/api/players/{id}"
-response = requests.get(url)
-data = response.json()
-player_id = data['profile']['account_id']
-print(player_id)
